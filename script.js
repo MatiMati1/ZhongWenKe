89d7c1ad6b0745d6edd652e0680978bdcd03250e
MatiMati1 <112687176+MatiMati1@users.noreply.github.com>
2023-12-16 12:53:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const side_menu = document.querySelector('.side-menu');
+const side_menu_toggle_lesson_button = document.querySelector('.side-menu-toggle');
+const side_menu_toggle_close_icon = document.querySelector('.side-menu-close-icon');
+
+function side_menu_toggle() {
+    side_menu.classList.toggle('side-menu-active');
+}
+
+side_menu_toggle_lesson_button.addEventListener('click', side_menu_toggle);
+side_menu_toggle_close_icon.addEventListener('click', side_menu_toggle); //blok kodu służący do otwierania i zamykania menu bocznego
+
+function togglesound(audioFile) {
+    const audio = new Audio(audioFile);
+
+    audio.play();
+}
